ee1e6fad92e2a19c8586cca72fa6cf498771ff4f
Emanuele Pavanello <e.pavanello@teamsystem.com>
2023-03-05 18:56:53 +0100

    mobile support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,21 +29,24 @@
 </div>
 
 <footer class="bg-neutral text-neutral-content">
-	<div class="footer p-4 w-full max-w-6xl mx-auto items-center">
-		<div class="items-center grid-flow-col">
-			<p>
-				Powered by <a href="https://openai.com/">OpenAI</a>,
-				<a href="https://vercel.com/">Vercel</a>,
-				<a href="https://supabase.com/">Supabase</a>,
-				<a href="https://kit.svelte.dev/">Svelte</a>,
-				<a href="https://tailwindcss.com/">Tailwind CSS</a>
-				- Copyright
-				<a href="https://github.com/epavanello" target="_blank" rel="noopener noreferrer"
-					>Pavanello Emanuele</a
-				> © 2023 - All right reserved
-			</p>
-		</div>
-		<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+	<div
+		class="p-4 w-full max-w-6xl mx-auto flex flex-row items-center justify-between mobile:flex-col mobile:gap-4"
+	>
+		<p class="text-center">
+			Copyright
+			<a href="https://github.com/epavanello" target="_blank" rel="noopener noreferrer"
+				>Pavanello Emanuele</a
+			> © 2023 - All right reserved
+		</p>
+		<p class="mobile:text-center">
+			Powered by <a href="https://openai.com/">OpenAI</a>,
+			<a href="https://vercel.com/">Vercel</a>,
+			<a href="https://supabase.com/">Supabase</a>,
+			<a href="https://kit.svelte.dev/">Svelte</a>,
+			<a href="https://tailwindcss.com/">Tailwind CSS</a>
+		</p>
+
+		<div class="flex flex-row gap-4">
 			<a href="https://twitter.com/e_pavanello" target="_blank" rel="noopener noreferrer"
 				><TwitterIcon />
 			</a>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: 'media',
 	theme: {
+		screens: {
+			mobile: { min: '0', max: '640px' },
+			...defaultTheme.screens
+		},
 		extend: {}
 	},
 	plugins: [require('daisyui')]
