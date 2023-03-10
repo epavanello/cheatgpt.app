7dc5313a891d517cdc0074da86db2e1238ce0c9d
Emanuele Pavanello <e.pavanello@teamsystem.com>
2023-03-10 23:36:23 +0100

    fix

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -4,13 +4,15 @@ import { readable } from 'svelte/store';
 export const isDark = readable(
 	browser && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches,
 	function start(set) {
-		const listener = (event: MediaQueryListEvent) => {
-			set(event.matches);
-		};
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', listener);
+		if (browser) {
+			const listener = (event: MediaQueryListEvent) => {
+				set(event.matches);
+			};
+			window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', listener);
 
-		return function stop() {
-			window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', listener);
-		};
+			return function stop() {
+				window.matchMedia('(prefers-color-scheme: dark)').removeEventListener('change', listener);
+			};
+		}
 	}
 );
